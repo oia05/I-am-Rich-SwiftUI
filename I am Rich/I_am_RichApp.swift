4dbdd38d303d24b5feed8325dc92b49b01a1f711
omar assidi <omar.assidi@cmeoffshore.com>
2023-10-27 13:06:33 +0300

    added project to git

---
+++ I am Rich/I_am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_RichApp.swift
+//  I am Rich
+//
+//  Created by OmarAssidi on 26/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct I_am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
